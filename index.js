19a5df2b420d18b5ba250944ab210ddf194c8342
cobyanderson <samuelcobyanderson@gmail.com>
2016-05-06 16:06:03 -0700

    switched order

--- index.js
+++ index.js
@@ -25,12 +25,12 @@ var api = new ParseServer({
      {
       pfx: '/Users/samuelcobyanderson/Documents/SwiftProjects/parse-server-legacycup/ApplePushCertificateMicosParse.p12', // The filename of private key and certificate in PFX or PKCS12 format from disk  
       bundleId: 'com.coby.MiCos', // The bundle identifier associate with your app
-      production: false // Specifies which environment to connect to: Production (if true) or Sandbox
+      production: true // Specifies which environment to connect to: Production (if true) or Sandbox
     },
     {
       pfx: '/Users/samuelcobyanderson/Documents/SwiftProjects/parse-server-legacycup/ApplePushCertificateMicosParse.p12', // The filename of private key and certificate in PFX or PKCS12 format from disk  
       bundleId: 'com.coby.MiCos', // The bundle identifier associate with your app
-      production: true // Specifies which environment to connect to: Production (if true) or Sandbox 
+      production: false // Specifies which environment to connect to: Production (if true) or Sandbox 
     }
     ]
     }
