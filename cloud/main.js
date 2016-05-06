31e29aae6bc7825d396f30a77d96ad350553ea08
cobyanderson <samuelcobyanderson@gmail.com>
2016-05-06 16:24:03 -0700

    print statement

--- cloud/main.js
+++ cloud/main.js
@@ -1,8 +1,9 @@
 
 Parse.Cloud.define('sendNotification', function(req, res) {
   //needs emoji, arcs, class, legacy, awardee, awarder, message, awardeeId
+  console.log(req.params);
   Parse.Push.send({
-    where: req.params.installations,
+    where: (req.params.installations),
     data: {
       alert: req.params.note,
       badge: 1
