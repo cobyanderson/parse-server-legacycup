86964a03392c48d21a70c824f18b3c41e7fc2fbd
cobyanderson <samuelcobyanderson@gmail.com>
2016-05-06 20:40:07 -0700

    reverted?

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 
-var databaseUri = 'mongodb://cobyanderson:lumpa@ds013212.mlab.com:13212/legacy-cup-database';
-
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
-  cloud: process.env.CLOUD_CODE_MAIN || '/Users/samuelcobyanderson/Documents/SwiftProjects/parse-server-legacycup/cloud/main.js',
+  databaseURI: 'mongodb://cobyanderson:lumpa@ds013212.mlab.com:13212/legacy-cup-database',
+  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || 'legacykey', //Add your master key here. Keep it secret!
-  push: {
+  masterKey: process.env.MASTER_KEY || 'legacykey', //Add your master key here. Keep it secret!,
+  serverURL: process.env.SERVER_URL || 'https://minerva-legacy-cup.herokuapp.com/parse', //'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  clientKey: 'clientkey',
+  liveQuery: {
+    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+  },
+    push: {
     ios: [
       {
       pfx: '/Users/samuelcobyanderson/Desktop/PushCertificateParseServerMiCos.p12', // The filename of private key and certificate in PFX or PKCS12 format from disk  
@@ -25,13 +28,7 @@ var api = new ParseServer({
       production: false // Specifies which environment to connect to: Production (if true) or Sandbox
     }
     ]
-    },
-  serverURL: process.env.SERVER_URL || 'https://minerva-legacy-cup.herokuapp.com/parse', //'http://localhost:1337/parse',  // Don't forget to change to https if needed
-  clientKey: 'clientkey',
-  liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
-
+    }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
